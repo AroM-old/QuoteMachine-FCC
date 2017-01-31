4ba7d09400d7dd2975fde7879f970916666e3ccc
Algenis <algenisromero@gmail.com>
2017-01-30 21:21:04 -0600

    Testing the tweet button

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+
+  function tweet(url){
+    var win = window.open(url, '_blank');
+    win.focus();
+  }
   
   $("#getMessage").on("click", function(e){
     e.preventDefault(); //Prevent load default browser setting
@@ -7,11 +12,13 @@ $(document).ready(function() {
   $.ajax( {
       url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1', 
       success: function(data){
-        var post = data.shift();
-        var tweetURL = "https://twitter.com/intent/tweet?text=" + post.content; //tweet button quote
+       
         $('.message').html(post.content); //replace message class for content JSON information
         $('.author').html('- ' + post.title + ' -'); //replace author class for content JSON information
-        $('.twitter-share-button').attr('href', tweetURL); //replace twitter button href for new tweet.
+        $('.twitter-share-button').on('click', function(){
+          if(quote !== undefined){
+            tweet("https://twitter.com/intent/tweet?text=" + "\"" + quote + "\" " + author);
+          } //replace twitter button href for new tweet.
       },
       cache: false // do not storage a cache site in the browser to generate next quote.
     });     
