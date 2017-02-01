7c9344db04a0a5a3433b4c618a09ad76405838cb
Algenis <algenisromero@gmail.com>
2017-01-31 21:49:06 -0600

    Final project.
    FCC quote generator completed and user story accomplished.

--- js/index.js
+++ js/index.js
@@ -1,26 +1,41 @@
-$(document).ready(function() {
+//Call to API using ajax to retrieve json data.
+function getJson() {
+  $.ajax({
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous',
+    type: 'POST',
+    dataType: 'json',
+    success: function (json) {
+      quote = json.quote;
+      author = json.author;
+      randomQuote(); //call to the function
+    },
+    error: function (err) {
+      alert(err);
+    },
+    beforeSend: function (tokenKey) {
+      tokenKey.setRequestHeader("X-Mashape-Key", "1YPdes7aatmshME9DdlFNsQHJozIp1JyjQOjsnssL6CNFkTwrT");
+    }
+  });
 
-  function tweet(url){
-    var win = window.open(url, '_blank');
-    win.focus();
-  }
-  
-  $("#getMessage").on("click", function(e){
-    e.preventDefault(); //Prevent load default browser setting
-    
-    //Using ajax to generate a new quote every time user click the button.
-  $.ajax( {
-      url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1', 
-      success: function(data){
-       
-        $('.message').html(post.content); //replace message class for content JSON information
-        $('.author').html('- ' + post.title + ' -'); //replace author class for content JSON information
-        $('.twitter-share-button').on('click', function(){
-          if(quote !== undefined){
-            tweet("https://twitter.com/intent/tweet?text=" + "\"" + quote + "\" " + author);
-          } //replace twitter button href for new tweet.
-      },
-      cache: false // do not storage a cache site in the browser to generate next quote.
-    });     
+}
+//
+function randomQuote() {
+  $(".quote").html("\"" + quote + "\""); //Fill new quote text tag
+  $(".author").html(author); //Fill author name text tag
+}
+
+//Post the quote in twitter.
+function tweet(url) {
+  var win = window.open(url, '_blank');
+  win.focus();
+}
+
+//Button 
+$(document).ready(function () {
+  $("#getMessage").on("click", getJson);
+  $("#tweetMessage").on("click", function () {
+    if (quote !== undefined) {
+      tweet("https://twitter.com/intent/tweet?text=" + "\"" + quote + "\" " + author);
+    }
   });
 });
